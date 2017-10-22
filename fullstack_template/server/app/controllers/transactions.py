e62ecd6a8953f1e3cfbc65bd329d547a6bb26df6
luis-alves <lmoalves@gmail.com>
2017-10-22 13:41:51 +0100

    Final touch to the body

--- fullstack_template/server/app/controllers/transactions.py
+++ fullstack_template/server/app/controllers/transactions.py
@@ -11,18 +11,5 @@ def transactions():
     entries = db.entries
 
     all_lines = entries.find()
-    # print dumps(all_lines)
-
-    result = [{"category": "Inflow: Salary",
-               "account": "Bankinter",
-               "reconcile": "true",
-               "Memo": "",
-               "payee": "Omatapalo",
-               "flag": "grey",
-               "outflow": "0",
-               "date": "03/10/2017",
-               "_id": {"$oid": "59de5112a4e4740ed8b04fdc"},
-               "id": 0,
-               "inflow": "2000.30"}]
 
     return dumps(all_lines)
--- fullstack_template/static/js/components/MainBody/Body/Transactions.jsx
+++ fullstack_template/static/js/components/MainBody/Body/Transactions.jsx
@@ -9,50 +9,63 @@ import { fetchTransactions } from "../../../actions/transactionsActions"
   }
 })
 export default class Transactions extends React.Component {
+  constructor(props) {
+    super(props)
+    this.test = {}
+  }
 
   componentWillMount () {
     this.props.dispatch(fetchTransactions())
   }
 
   render () {
-    // const { transactions } = this.props
-      let arr = []
-      arr.push(this.props.transactions)
-      const mapped = arr.map( (row, i=0) => {
-      if (row) {
-        return (
-          <div className="article-row" key={i}>
-            <div className="table-header">
-            <div className="checkboxOne">
+    if(this.props.transactions != null) {
+      const rows = this.props.transactions.map((row) =>
+         <div className="table-header" key={row.id}>
+           <div className="checkboxOne">
              <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
              <label htmlFor="checkboxOneInput"></label>
-            </div>
-            <div className="boxing-info">
-              <i className="icon-info not-present"></i>
-            </div>
-            <div className="boxing-info">
-              <i className="icon-bookmark unchecked"></i>
-            </div>
-            <h5 className="date">{row[i]['date']}</h5>
-            <h5 className="payee">{row[i]['payee']}</h5>
-            <h5 className="category">{row[i]['category']}</h5>
-            <h5 className="memo">{row[i]['Memo']}</h5>
-            <h5 className="outflow">{row[i]['outflow']}</h5>
-            <h5 className="inflow">{row[i]['inflow']}</h5>
-            <div className="boxing-reconcile">
-              <i title="Reconcile account" className="icon-check unchecked"></i>
-            </div>
-          </div>
-          </div>
+           </div>
+           <div className="boxing-info">
+             <i className="icon-info not-present"></i>
+           </div>
+           <div className="boxing-info">
+             <i className="icon-bookmark unchecked"></i>
+           </div>
+           <h5 className="date">{row.date}</h5>
+           <h5 className="payee">{row.payee}</h5>
+           <h5 className="category">{row.category}</h5>
+           <h5 className="memo">{row.Memo}</h5>
+           <h5 className="outflow">{row.outflow}</h5>
+           <h5 className="inflow">{row.inflow}</h5>
+           <div className="boxing-reconcile">
+             <i title="Reconcile account" className="icon-check unchecked"></i>
+           </div>
+         </div>
       )
-    }
-
-      })
       return (
         <div>
-          <ul>{mapped}</ul>
+          <div className="article-row">{rows}</div>
         </div>
       )
+    }
+    return (
+      <div>
+        <h2>No transactions data</h2>
+      </div>
+    )
+  }
+
+        // }
+
+}
+
+
+      // return (
+      //   <div>
+      //     <ul>{mapped}</ul>
+      //   </div>
+      // )
 
 
     // for (var row in this.props.transactions) {
@@ -88,7 +101,3 @@ export default class Transactions extends React.Component {
     //
     //   }
     // }
-
-
-  }
-}
