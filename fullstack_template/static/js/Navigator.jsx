e51bb243e8781a3ac236ea738593b330d33c9e3c
Luis Alves <lmoalves@gmail.com>
2017-10-22 22:35:38 +0100

    Event listenner added

--- fullstack_template/static/js/Navigator.jsx
+++ fullstack_template/static/js/Navigator.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+// import {
+//     Navbar,
+//     Nav,
+//     NavItem,
+//     NavDropdown,
+//     MenuItem
+// } from "react-bootstrap";
 import {
-    Navbar,
-    Nav,
-    NavItem,
-    NavDropdown,
-    MenuItem
-} from "react-bootstrap";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import { LinkContainer } from "react-router-bootstrap";
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
+// import {
+//     LinkContainer
+// } from "react-router-bootstrap";
 
 import Menu from "./components/Navbar/Menu"
 import AccountsMenu from "./components/Navbar/AccountsMenu"
 
 
-export default class Navigator extends React.Component{
-  constructor(props) {
-    super(props);
-  }
-  render () {
-    return (
-      <div>
+export default class Navigator extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
         <nav className="navbar">
           <ul>
             <Menu />
@@ -27,7 +32,7 @@ export default class Navigator extends React.Component{
           </ul>
         </nav>
       </div>
-    );
-  }
+        );
+    }
 
 }
--- fullstack_template/static/js/components/MainBody/Body/Transactions.jsx
+++ fullstack_template/static/js/components/MainBody/Body/Transactions.jsx
@@ -1,103 +1,111 @@
 import React from "react"
-import { connect } from "react-redux"
+import {
+    connect
+} from "react-redux"
 
-import { fetchTransactions } from "../../../actions/transactionsActions"
+import {
+    fetchTransactions
+} from "../../../actions/transactionsActions"
 
 @connect(store => {
-  return {
-    transactions: store.transactions.transactions
-  }
+    return {
+        transactions: store.transactions.transactions
+    }
 })
 export default class Transactions extends React.Component {
-  constructor(props) {
-    super(props)
-    this.test = {}
-  }
+    constructor(props) {
+        super(props)
+        this.changeColor = this.changeColor.bind(this)
+    }
 
-  componentWillMount () {
-    this.props.dispatch(fetchTransactions())
-  }
+    componentWillMount() {
+        this.props.dispatch(fetchTransactions())
+    }
 
-  render () {
-    if(this.props.transactions != null) {
-      const rows = this.props.transactions.map((row) =>
-         <div className="table-header" key={row.id}>
-           <div className="checkboxOne">
-             <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
-             <label htmlFor="checkboxOneInput"></label>
-           </div>
-           <div className="boxing-info">
-             <i className="icon-info not-present"></i>
-           </div>
-           <div className="boxing-info">
-             <i className="icon-bookmark unchecked"></i>
-           </div>
-           <h5 className="date">{row.date}</h5>
-           <h5 className="payee">{row.payee}</h5>
-           <h5 className="category">{row.category}</h5>
-           <h5 className="memo">{row.Memo}</h5>
-           <h5 className="outflow">{row.outflow}</h5>
-           <h5 className="inflow">{row.inflow}</h5>
-           <div className="boxing-reconcile">
-             <i title="Reconcile account" className="icon-check unchecked"></i>
-           </div>
-         </div>
-      )
-      return (
-        <div>
+    changeColor(e) {
+        alert(e.target.parentElement.parentElement.id);
+    }
+
+    render() {
+        if (this.props.transactions != null) {
+            const rows = this.props.transactions.map((row) =>
+                <div className="table-header" key={row._id['$oid']} id={row._id['$oid']}>
+                   <div className="checkboxOne">
+                     <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
+                     <label htmlFor="checkboxOneInput"></label>
+                   </div>
+                   <div className="boxing-info">
+                     <i className="icon-info not-present"></i>
+                   </div>
+                   <div className="boxing-info">
+                     <i className="icon-bookmark unchecked"></i>
+                   </div>
+                   <h5 className="date">{row.date}</h5>
+                   <h5 className="payee">{row.payee}</h5>
+                   <h5 className="category">{row.category}</h5>
+                   <h5 className="memo">{row.Memo}</h5>
+                   <h5 className="outflow">{row.outflow}</h5>
+                   <h5 className="inflow">{row.inflow}</h5>
+                   <div className="boxing-reconcile" id="parente">
+                     <i title="Reconcile account" id="filho" className="icon-check unchecked" onClick={this.changeColor}></i>
+                   </div>
+                 </div>
+            )
+            return (
+                <div>
           <div className="article-row">{rows}</div>
         </div>
-      )
-    }
-    return (
-      <div>
+            )
+        }
+        return (
+            <div>
         <h2>No transactions data</h2>
       </div>
-    )
-  }
+        )
+    }
 
-        // }
+    // }
 
 }
 
 
-      // return (
-      //   <div>
-      //     <ul>{mapped}</ul>
-      //   </div>
-      // )
+// return (
+//   <div>
+//     <ul>{mapped}</ul>
+//   </div>
+// )
 
 
-    // for (var row in this.props.transactions) {
-    //   console.log(row);
-    //   if (this.props.transactions.hasOwnProperty(row)) {
-    //
-    //       <div className="article-row">
-    //
-    //         <div className="table-header">
-    //           <div className="checkboxOne">
-    //             <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
-    //             <label htmlFor="checkboxOneInput"></label>
-    //           </div>
-    //           <div className="boxing-info">
-    //             <i className="icon-info not-present"></i>
-    //           </div>
-    //           <div className="boxing-info">
-    //             <i className="icon-bookmark unchecked"></i>
-    //           </div>
-    //           {console.log(row.category)}
-    //           <h5 className="date">{this.props.transactions}</h5>
-    //           {/* <h5 className="payee">{this.props.transactions['payee']}</h5> */}
-    //           {/* <h5 className="category">{this.props.transactions['category']}</h5>
-    //           <h5 className="memo">{this.props.transactions['Memo']}</h5>
-    //           <h5 className="outflow">{this.props.transactions['outflow']}</h5>
-    //           <h5 className="inflow">{this.props.transactions['inflow']}</h5> */}
-    //           <div className="boxing-reconcile">
-    //             <i title="Reconcile account" className="icon-check unchecked"></i>
-    //           </div>
-    //         </div>
-    //
-    //       </div>
-    //
-    //   }
-    // }
+// for (var row in this.props.transactions) {
+//   console.log(row);
+//   if (this.props.transactions.hasOwnProperty(row)) {
+//
+//       <div className="article-row">
+//
+//         <div className="table-header">
+//           <div className="checkboxOne">
+//             <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
+//             <label htmlFor="checkboxOneInput"></label>
+//           </div>
+//           <div className="boxing-info">
+//             <i className="icon-info not-present"></i>
+//           </div>
+//           <div className="boxing-info">
+//             <i className="icon-bookmark unchecked"></i>
+//           </div>
+//           {console.log(row.category)}
+//           <h5 className="date">{this.props.transactions}</h5>
+//           {/* <h5 className="payee">{this.props.transactions['payee']}</h5> */}
+//           {/* <h5 className="category">{this.props.transactions['category']}</h5>
+//           <h5 className="memo">{this.props.transactions['Memo']}</h5>
+//           <h5 className="outflow">{this.props.transactions['outflow']}</h5>
+//           <h5 className="inflow">{this.props.transactions['inflow']}</h5> */}
+//           <div className="boxing-reconcile">
+//             <i title="Reconcile account" className="icon-check unchecked"></i>
+//           </div>
+//         </div>
+//
+//       </div>
+//
+//   }
+// }
