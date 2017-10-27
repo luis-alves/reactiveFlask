6b679722474feb78908c4f3662fc5eaa478165e1
luis-alves <lmoalves@gmail.com>
2017-10-27 14:45:43 +0100

    Creation of a Modal to insert colored bookmarks

--- fullstack_template/static/js/components/MainBody/Body/Transactions.jsx
+++ fullstack_template/static/js/components/MainBody/Body/Transactions.jsx
@@ -6,7 +6,27 @@ import {
 import {
     fetchTransactions, updateTransactions
 } from "../../../actions/transactionsActions"
+import Modal from "react-modal"
 
+var modalStyles = {
+  overlay : {
+    color: 'green',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content : {
+    left: '300px',
+    margin: '0.5rem',
+    padding: '1rem',
+    outline: 'none',
+    overflow: 'auto',
+    backgroundColor: 'white',
+    boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.24), 0 16px 40px 0 rgba(0, 0, 0, 0.32)',
+  }
+}
 
 @connect(store => {
     return {
@@ -16,56 +36,81 @@ import {
 export default class Transactions extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {isActive: false}
         this.changeColor = this.changeColor.bind(this)
+        this.setRow = this.setRow.bind(this)
     }
 
     componentWillMount() {
         this.props.dispatch(fetchTransactions())
+        Modal.setAppElement('#content')
     }
 
     changeColor(e) {
       this.props.dispatch(updateTransactions(e))
     }
 
+    // toggleModal = () => {
+    //   this.setState({isActive: !this.state.isActive})
+    // }
+    setRow (row, event) {
+      console.log({row});
+      this.setState({row: event})
+    }
+
+
     render() {
-        if (this.props.transactions != null) {
-            const rows = this.props.transactions.map((row) =>
-                <div className="table-header handhover" key={row._id['$oid']} id={row._id['$oid']}>
-                   <div className="checkboxOne handhover">
-                     <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
-                     <label htmlFor="checkboxOneInput"></label>
-                   </div>
-                   <div className="boxing-info handhover">
-                     <i className={"icon-info handhover " + row.flag}></i>
-                   </div>
-                   <div className="boxing-info handhover">
-                     <i className={"icon-bookmark handhover " + row.bookmark}></i>
-                   </div>
-                   <h5 className="date handhover">{row.date}</h5>
-                   <h5 className="payee handhover">{row.payee}</h5>
-                   <h5 className="category handhover">{row.category}</h5>
-                   <h5 className="memo handhover">{row.Memo}</h5>
-                   <h5 className="outflow handhover">{row.outflow}</h5>
-                   <h5 className="inflow handhover">{row.inflow}</h5>
-                   <div className="boxing-reconcile handhover" id="parente">
-                     <i
-                       title="Reconcile account"
-                       id="filho"
-                       className={"icon-check handhover " + row.reconcile}
-                       onClick={this.changeColor}></i>
-                   </div>
+
+
+      if (this.props.transactions != null) {
+          const rows = this.props.transactions.map((row) =>
+              <div className="table-header handhover" key={row._id['$oid']} id={row._id['$oid']}>
+                 <div className="checkboxOne handhover">
+                   <input type="checkbox" name="" id="checkboxOneInput" value="1"/>
+                   <label htmlFor="checkboxOneInput"></label>
                  </div>
-            )
-            return (
-                <div>
-          <div className="article-row">{rows}</div>
-        </div>
-            )
-        }
-        return (
-            <div>
-        <h2>No transactions data</h2>
+                 <div className="boxing-info handhover">
+                   <i className={"icon-info handhover " + row.flag}></i>
+                 </div>
+                 <div className="boxing-info handhover">
+                   <i className={"icon-bookmark handhover " + row.bookmark}
+                      onClick={this.setRow.bind(row, event)}>
+
+                    </i>
+                 </div>
+                 <h5 className="date handhover">{row.date}</h5>
+                 <h5 className="payee handhover">{row.payee}</h5>
+                 <h5 className="category handhover">{row.category}</h5>
+                 <h5 className="memo handhover">{row.Memo}</h5>
+                 <h5 className="outflow handhover">{row.outflow}</h5>
+                 <h5 className="inflow handhover">{row.inflow}</h5>
+                 <div className="boxing-reconcile handhover" id="parente">
+                   <i
+                     title="Reconcile account"
+                     id="filho"
+                     className={"icon-check handhover " + row.reconcile}
+                     onClick={this.changeColor}></i>
+                 </div>
+               </div>
+          )
+          return (
+              <div>
+        <div className="article-row">{rows}</div>
+        <Modal isOpen={this.state.row != null}
+               onRequestClose={this.setRow.bind(null)}
+               style={modalStyles}
+              //  className='modal-content-color'
+              //  overlayClassName='modal-overlay-color'
+        >
+          <h1>EHlloo</h1>
+        </Modal>
       </div>
-        )
+          )
+      }
+      return (
+          <div>
+      <h2>No transactions data</h2>
+    </div>
+      )
     }
 }
